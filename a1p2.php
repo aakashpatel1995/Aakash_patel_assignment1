d9591ce6ecba405df79e92db28096ec2c30439a3
aakashpatel1995 <58455804+aakashpatel1995@users.noreply.github.com>
2020-01-29 10:56:43 -0500

    a1p2 file from assignment file

--- a1p2.php
+++ a1p2.php
@@ -1,18 +1,36 @@
 /* Aakash Patel */
 /* ID - 8622564 */
-<html>
-<head><title>Assignment 1</title></head>
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Simple HTML Form</title>
+	<style type="text/css">
+	label {
+		font-weight: bold;
+		color: #300ACC;
+	}
+	</style>
+</head>
 <body>
-<form action="a1p2.php" method="post">/* etext -37 */
-<fieldset><legend>Enter Your Details Below</legend>/* etext -37 */
-<p><label>firstname: <input type="text" name="firstname" size="20" maxlength="40"></label></p>/* etext -37 */
-<p><label>lastname: <input type="text" name="lastname" size="20" maxlength="40"></label></p>/* etext -37 */
-</fieldset>
-<p align ="center"><input type = "submit" name = "submit" value ="Submit My Information"></p>
+
+<form method="post">
+
+	<fieldset><legend>Account Creation:</legend>
+
+	<p><label>First Name: <input type="text" name="firstname" size="20" maxlength="40"></label></p>
+	
+	<p><label>Last Name: <input type="text" name="lastname" size="20" maxlength="40"></label></p>
+
+	<p><label>Age: <input type="text" name="age" size="20" maxlength="40"></label></p>
+	
+	<p><label for="newsletter">Subscribe to newsletter: </label><input type="radio" name="newsletter" value="Y"> Yes <input type="radio" name="newsletter" value="N"> No</p>
+
+	</fieldset>
+
+	<p align="center"><input type="submit" name="submit" value="Submit My Information"></p>
 
 </form>
 
 </body>
-
-
 </html>
