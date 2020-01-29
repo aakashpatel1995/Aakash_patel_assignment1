d012ba4eeb7ee82e3179ac47007f2ee06e64f621
aakashpatel1995 <58455804+aakashpatel1995@users.noreply.github.com>
2020-01-29 01:35:57 -0500

    php file earlier was text

---
+++ chapter1.php
@@ -0,0 +1,10 @@
+<?php
+$base = 3;   /* etext page -14 */
+$height = 7;
+$area = (1/2($base * $height));
+$area = number_format($area, 3);/* etext page 24 */
+echo '<p>The area of the Triangle is ' . $area .'</p>'
+define('MY_NAME','Aakash Patel');/* etext page 26 */
+$MY_NAME = strtoupper(MY_NAME);/* etext page 22 */
+echo '<p> My first name is <strong>'. $MY_NAME .'</strong></p>';/* etext page 27 */
+?>
