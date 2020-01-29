4d72bcd3544142fbb5df58fdef8efb05e7dc769e
aakashpatel1995 <58455804+aakashpatel1995@users.noreply.github.com>
2020-01-29 02:08:25 -0500

    created form
    Created form will work on validation

---
+++ a1p2.php
@@ -0,0 +1,18 @@
+/* Aakash Patel */
+/* ID - 8622564 */
+<html>
+<head><title>Assignment 1</title></head>
+<body>
+<form action="a1p2.php" method="post">/* etext -37 */
+<fieldset><legend>Enter Your Details Below</legend>/* etext -37 */
+<p><label>firstname: <input type="text" name="firstname" size="20" maxlength="40"></label></p>/* etext -37 */
+<p><label>lastname: <input type="text" name="lastname" size="20" maxlength="40"></label></p>/* etext -37 */
+</fieldset>
+<p align ="center"><input type = "submit" name = "submit" value ="Submit My Information"></p>
+
+</form>
+
+</body>
+
+
+</html>
