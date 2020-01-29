23d0ee0b6411c261887024e27337587b51aa8ce3
aakashpatel1995 <58455804+aakashpatel1995@users.noreply.github.com>
2020-01-29 11:29:34 -0500

    sticky input  validation left.

--- a1p2.php
+++ a1p2.php
@@ -13,24 +13,20 @@
 	</style>
 </head>
 <body>
+<?php
 
-<form method="post">
-
+?>
+<form method="post" action ="a1p2.php">
 	<fieldset><legend>Account Creation:</legend>
-
-	<p><label>First Name: <input type="text" name="firstname" size="20" maxlength="40"></label></p>
+	<p><label>First Name: <input type="text" name="firstname" size="20" maxlength="40"
+    value="<? php if(isset ($_POST['firstname']))echo$_POST['firstname'];?>"></label></p>
 	
-	<p><label>Last Name: <input type="text" name="lastname" size="20" maxlength="40"></label></p>
-
-	<p><label>Age: <input type="text" name="age" size="20" maxlength="40"></label></p>
+	<p><label>Last Name: <input type="text" name="lastname" size="20" maxlength="40"value="<? php if(isset ($_POST['lastname']))echo$_POST['lastname'];?>"></label></p>
+	<p><label>Age: <input type="text" name="age" size="20" maxlength="40"value="<? php if(isset ($_POST['age']))echo$_POST['age'];?>"></label></p>
 	
 	<p><label for="newsletter">Subscribe to newsletter: </label><input type="radio" name="newsletter" value="Y"> Yes <input type="radio" name="newsletter" value="N"> No</p>
-
 	</fieldset>
-
 	<p align="center"><input type="submit" name="submit" value="Submit My Information"></p>
-
 </form>
-
 </body>
 </html>
